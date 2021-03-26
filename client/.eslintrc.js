5968e3278121958fb665b321315fbbfff476b2fa
unknown <nbarmenkov@gmail.com>
2021-03-26 18:42:28 +0300

    build: add linter
    Add ESLint airbnb config to the server and client folders.

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -2,14 +2,15 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   extends: [
     '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended'
+    'plugin:vue/essential',
+    '@vue/airbnb',
   ],
   plugins: [
   ],
   // add your custom rules here
-  rules: {}
-}
+  rules: {},
+};
